96b4ab9977a6a0aea2dbf2b33f17ef67b317c2c3
Albert Albala <albert.albala@linuxfoundation.org>
2015-05-21 08:23:52 -0400

    Remove curl until we need it, and add nginx

--- dcyclebox/test.sh
+++ dcyclebox/test.sh
@@ -1,5 +1,12 @@
+#!/bin/sh
+# set -e makes sure that this script will fail if any of its lines fail
+set -e
+
 # Start by building the box
 docker build -t dcyclebox-test .
 
-# Test CURL
-docker run dcyclebox-test bash -c 'which curl' && echo '[test] curl works'
+# Test nginx
+docker run dcyclebox-test bash -c 'nginx -v'
+echo '[test] nginx works'
+
+echo '[status] end of test script'
