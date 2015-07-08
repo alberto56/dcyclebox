861831f1af8ccfd9b102547432630133556d424d
Albert Albala <albert.albala@linuxfoundation.org>
2015-07-07 21:50:20 -0400

    Added script to increment version

---
+++ scripts/increment-version.sh
@@ -0,0 +1,7 @@
+CURRENTVERSION=$(head -n 1 move-to-your-project-root/jekyll/dcycle/CHANGELOG.txt|sed s/Version\ //)
+NEWVERSION=$(expr $CURRENTVERSION + 1)
+echo "Version $NEWVERSION" > move-to-your-project-root/drupal7module/dcycle/CHANGELOG.txt
+echo "Version $NEWVERSION" > move-to-your-project-root/drupal7site/dcycle/CHANGELOG.txt
+echo "Version $NEWVERSION" > move-to-your-project-root/drupal8module/dcycle/CHANGELOG.txt
+echo "Version $NEWVERSION" > move-to-your-project-root/drupal8site/dcycle/CHANGELOG.txt
+echo "Version $NEWVERSION" > move-to-your-project-root/jekyll/dcycle/CHANGELOG.txt
