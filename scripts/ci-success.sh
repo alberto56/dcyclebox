2249d60ecc225c84e6e1af5b81a27521d240e263
Albert Albala <albert.albala@linuxfoundation.org>
2015-07-07 22:08:59 -0400

    Specify merge message

--- scripts/ci-success.sh
+++ scripts/ci-success.sh
@@ -3,9 +3,9 @@ if [ $(git rev-parse --abbrev-ref HEAD) == 'develop' ] ; then
   git config --global user.name "alberto56's CircleCI build"
   git checkout master
   git pull origin master
-  git merge develop
   CURRENTVERSION=$(head -n 1 move-to-your-project-root/jekyll/dcycle/CHANGELOG.txt|sed s/Version\ //)
   NEWVERSION=$(expr $CURRENTVERSION + 1)
+  git merge develop -m "Merging the develop branch because it passed at version $NEWVERSION"
   echo "Version $NEWVERSION" > move-to-your-project-root/drupal7module/dcycle/CHANGELOG.txt
   echo "Version $NEWVERSION" > move-to-your-project-root/drupal7site/dcycle/CHANGELOG.txt
   echo "Version $NEWVERSION" > move-to-your-project-root/drupal8module/dcycle/CHANGELOG.txt
