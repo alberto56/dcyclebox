dc37dcf6e9646b119f642e59cc2301278ce3a6bf
Albert Albala <albert.albala@linuxfoundation.org>
2015-07-07 21:54:37 -0400

    New versioning script

--- scripts/increment-version.sh
+++ scripts/increment-version.sh
@@ -1,7 +1,9 @@
-CURRENTVERSION=$(head -n 1 move-to-your-project-root/jekyll/dcycle/CHANGELOG.txt|sed s/Version\ //)
-NEWVERSION=$(expr $CURRENTVERSION + 1)
-echo "Version $NEWVERSION" > move-to-your-project-root/drupal7module/dcycle/CHANGELOG.txt
-echo "Version $NEWVERSION" > move-to-your-project-root/drupal7site/dcycle/CHANGELOG.txt
-echo "Version $NEWVERSION" > move-to-your-project-root/drupal8module/dcycle/CHANGELOG.txt
-echo "Version $NEWVERSION" > move-to-your-project-root/drupal8site/dcycle/CHANGELOG.txt
-echo "Version $NEWVERSION" > move-to-your-project-root/jekyll/dcycle/CHANGELOG.txt
+if [ $(git rev-parse --abbrev-ref HEAD) == 'develop' ] ; then
+  CURRENTVERSION=$(head -n 1 move-to-your-project-root/jekyll/dcycle/CHANGELOG.txt|sed s/Version\ //)
+  NEWVERSION=$(expr $CURRENTVERSION + 1)
+  echo "Version $NEWVERSION" > move-to-your-project-root/drupal7module/dcycle/CHANGELOG.txt
+  echo "Version $NEWVERSION" > move-to-your-project-root/drupal7site/dcycle/CHANGELOG.txt
+  echo "Version $NEWVERSION" > move-to-your-project-root/drupal8module/dcycle/CHANGELOG.txt
+  echo "Version $NEWVERSION" > move-to-your-project-root/drupal8site/dcycle/CHANGELOG.txt
+  echo "Version $NEWVERSION" > move-to-your-project-root/jekyll/dcycle/CHANGELOG.txt
+fi
