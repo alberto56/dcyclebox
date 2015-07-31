b25364f70e8140df40dc88c4c70892171b701c93
Albert Albala <albert.albala@linuxfoundation.org>
2015-07-30 23:23:50 -0400

    Attempted fix for failing build

--- tests/lib/test-drupal6site-run.sh
+++ tests/lib/test-drupal6site-run.sh
@@ -2,12 +2,11 @@
 set -e
 echo -e "[  >>] Start of script $0 (from $(pwd))"
 
-PORT=7290
+PORT=7233
 
 TESTID=$(./tests/lib/util/test-id.sh)
 
 cp -r ./copy-to-your-project-root/drupal6site ./tests/tmp/.
-cp -r ./tests/lib/files/drupal6site/* ./tests/tmp/drupal6site/.
 cd ./tests/tmp/drupal6site
 echo -e '[info] ./tests/tmp/drupal6site should now contain bare-bones d6 site structure for testing'
 ls -lah
